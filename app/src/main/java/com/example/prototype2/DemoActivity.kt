aeb1dd5bbf8f92eb6fa2d3813f916e4e0474cfc8
Raihan Maharaja <raihan.maharaja.tkom419@polban.ac.id>
2022-10-25 16:44:30 +0700

    belajarandroid

--- app/src/main/java/com/example/prototype2/DemoActivity.kt
+++ app/src/main/java/com/example/prototype2/DemoActivity.kt
@@ -3,13 +3,18 @@ package com.example.prototype2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -17,7 +22,10 @@ class DemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            Column {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)) {
+                Image(painter = painterResource(id = R.drawable.wakanda), contentDescription = "")
                 Surface(color = Color.Blue, modifier = Modifier.fillMaxWidth()) {
                     Text(text = "Hello Polban!",
                         fontSize = 24.sp,
